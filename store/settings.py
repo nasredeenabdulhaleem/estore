45682d6d7b667602497dd24ea828173fb08ecba4
Abdulhaleem <nabdulhaleem09@gmail.com>
2024-01-09 08:54:26 +0100

    Add MEDIA_ROOT to settings.py

--- store/settings.py
+++ store/settings.py
@@ -188,6 +188,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # STATIC_URL = "static/"
 # MEDIA_URL = "/media/"
 
